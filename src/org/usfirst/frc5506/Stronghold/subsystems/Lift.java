6762d84f4beb4285ff421ce464d4e20241470f04
Tupster24 <tupperkion@gmail.com>
2016-01-26 18:02:38 -0500

    Finished OI, added LiftTeleop and Shoot commands

--- src/org/usfirst/frc5506/Stronghold/subsystems/Lift.java
+++ src/org/usfirst/frc5506/Stronghold/subsystems/Lift.java
@@ -45,5 +45,9 @@ public class Lift extends Subsystem {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
     }
+    
+    public void setSpeed(double speed) {
+    	liftMotor.set(speed);
+    }
 }
 
