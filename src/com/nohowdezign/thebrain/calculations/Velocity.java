b4ebf44e1743d4339bc66d478fe650c1953c3c3d
nh-99 <ifence9@gmail.com>
2016-03-22 08:04:16 -0400

    Remove old autonomous stuff, add in new routine (drive forward at half speed for 5 seconds)

--- src/com/nohowdezign/thebrain/calculations/Velocity.java
+++
@@ -1,9 +0,0 @@
-package com.nohowdezign.thebrain.calculations;
-
-public class Velocity {
-	
-	public double getVelocityForDistance(double distance, double height, double time) {
-		return Math.sqrt(Math.pow(distance, 2) - Math.pow(height, 2));
-	}
-	
-}
--- src/com/nohowdezign/thebrain/server/PrintServer.java
+++
@@ -1,9 +0,0 @@
-package com.nohowdezign.thebrain.server;
-
-public class PrintServer extends ServerFactory {
-	
-	public void onMessage(String message) {
-		System.out.println(message);
-	}
-	
-}
